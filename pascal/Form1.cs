4c78cf9f3a733ddc037c580656cd57acf29888ad
zsofikozma <kozma.zsofia0616@gmail.com>
2024-03-07 11:04:56 +0100

    cringe

---
+++ pascal/Form1.cs
@@ -0,0 +1,35 @@
+namespace pascal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int sor = 0; sor < 10; sor++)
+            {
+                for (int oszlop = 0; oszlop < 10; oszlop++)
+                {
+                    Button b = new Button();
+                    b.Top = sor * 60;
+                    b.Left = oszlop * 60;
+                    b.Height = 60;
+                    b.Width = 60;
+                    this.Controls.Add(b);
+                    int p = Faktorialis(sor) / (Faktorialis(oszlop) * (Faktorialis(sor - oszlop)));
+                    b.Text = p.ToString();
+                }
+            }
+
+            int Faktorialis(int n)
+            {
+                int eredmeny = 1;
+                for (int i = 1; i <= n; i++) eredmeny *= i;
+                return eredmeny;
+            }
+        }
+    }
+}
